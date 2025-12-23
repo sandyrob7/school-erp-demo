ecc8265681f062bd39a2795123dc5318cfb8f51c
sandyrob7 <sandyrob7@gmail.com>
2025-12-23 09:54:28 +0000

    Fix globals.css import path

--- next.config.js
+++ next.config.js
@@ -2,7 +2,6 @@ const path = require('path');
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: { appDir: true },
   webpack: (config) => {
     config.resolve.alias = {
       ...(config.resolve.alias || {}),
